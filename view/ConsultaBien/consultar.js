bb9e9b8908416dab29578671b900ac4cd168193f
EtorresCh <edgarjtorreschafloque3@gmail.com>
2025-09-11 11:53:27 -0500

    Version 21.0 - Version Final 11/09/25

--- view/ConsultaBien/consultar.js
+++ view/ConsultaBien/consultar.js
@@ -36,30 +36,37 @@ function buscarBien() {
             confirmButtonText: "Aceptar",
             confirmButtonColor: 'rgb(243, 18, 18)',
           });
-         $(".respuesta").html(`
-          <div class="alert alert-warning" role="alert">
-            <div class="d-flex">
-              <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon icon-alert mx-1" width="24" height="24" 
-                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" 
-                    stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M12 9v2m0 4v.01" />
-                  <path d="M12 5a7 7 0 1 0 0 14a7 7 0 0 0 0 -14z" />
-                </svg>
-              </div>
-              <div>
-                <h4 class="alert-title mx-3">Bien No Encontrado</h4>
-                <div class="text-dark mx-3">
-                  No se ha encontrado ningún registro asociado al <strong>código de barras ingresado</strong>. Asegúrese de haber <strong>ingresado un código válido</strong> o <strong>escaneado correctamente</strong> el bien patrimonial.  
-                  <br>Una vez se detecte un código válido, se mostrará automáticamente el <strong>detalle completo del bien</strong>, incluyendo su <strong>identificación</strong>, <strong>estado</strong>, <strong>valor</strong> y <strong>responsable asignado</strong>.
+          $(".respuesta").html(`
+            <div class="alert alert-warning" role="alert">
+              <div class="d-flex flex-column flex-md-row align-items-center">
+                <div class="mb-2 mb-md-0">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon icon-alert mx-2" width="24" height="24" 
+                      viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" 
+                      stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M12 9v2m0 4v.01" />
+                    <path d="M12 5a7 7 0 1 0 0 14a7 7 0 0 0 0 -14z" />
+                  </svg>
                 </div>
-
+                <div class="text-center text-md-start">
+                  <h4 class="alert-title mx-2">Bien No Encontrado</h4>
+                  <div class="text-dark mx-2">
+                    No se ha encontrado ningún registro asociado al <strong>código de barras ingresado</strong>. 
+                    Asegúrese de haber <strong>ingresado un código válido</strong> o <strong>escaneado correctamente</strong> el bien patrimonial.  
+                    <br>Una vez se detecte un código válido, se mostrará automáticamente el <strong>detalle completo del bien</strong>, incluyendo su <strong>identificación</strong>, <strong>estado</strong>, <strong>valor</strong> y <strong>responsable asignado</strong>.
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <img id="cargando-detalle" src="../../public/logo_mpch2.png" alt="Cargando..." style="width: 380px; height: 380px; margin-left: 50vh; margin-top: 40px; margin-bottom:40px;">
-        `);
+
+            <div class="text-center my-4">
+              <img id="cargando-detalle" src="../../public/logo_mpch2.png" 
+                  alt="Cargando..." 
+                  class="img-fluid" 
+                  style="max-width: 380px; height: auto;">
+            </div>
+          `);
+
 
           return;
         }
